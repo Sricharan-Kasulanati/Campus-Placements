440f848811126723e8bb8df745f234b5c988db85
ue2much <summerwentaway@gmail.com>
2025-10-16 12:23:23 -0500

    change app.js file

--- Frontend/campus-placements-frontend/src/App.js
+++ Frontend/campus-placements-frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          test
+        </p>
       </header>
     </div>
   );
