fe971dd64ccefa9ca3e3d41a1f9efc339c38a555
Sricharan-Kasulanati <sricharankasulanati2001@gmail.com>
2025-11-27 12:30:18 -0600

    Securing the routes and adding pagination

--- Frontend/campus-placements-frontend/src/auth/ProtectedRoute.tsx
+++ Frontend/campus-placements-frontend/src/auth/ProtectedRoute.tsx
@@ -1,9 +1,19 @@
-import { Navigate } from 'react-router-dom';
-import type { PropsWithChildren } from 'react';
-import { useAuth } from './AuthProvider';
+import { Navigate } from "react-router-dom";
+import type { PropsWithChildren } from "react";
+import { useAuth } from "./AuthProvider";
 
-export default function ProtectedRoute({ children }: PropsWithChildren) {
-  const { token } = useAuth();
+type ProtectedProps = PropsWithChildren & {
+  role?: "ADMIN" | "STUDENT";
+};
+
+export default function ProtectedRoute({ children, role }: ProtectedProps) {
+  const { token, user } = useAuth();
+
   if (!token) return <Navigate to="/login" replace />;
+  if (role && user?.role !== role) {
+    return (
+      <Navigate to={user?.role === "ADMIN" ? "/admin" : "/landing"} replace />
+    );
+  }
   return <>{children}</>;
 }
